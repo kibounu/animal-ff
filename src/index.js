7ad1c49559e49eed2758a3601333e27cd3aced94
nadlam <send.kibou@gmail.com>
2022-04-15 16:38:20 +0800

    upload files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { animals } from './animals';
 
+// codes for animal fun facts
+const title = "";
+
+const background = <img src="/images/bg.jpg" className="background" alt="ocean" />;
+
+const images = [];
+
+for (const animal in animals) {
+  images.push(
+    <img
+      key={animal}
+      className="animal"
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role="button"
+      onClick={displayFact}
+    />
+  );
+}
+
+const animalFacts = (
+  <div>
+    <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>
+    {background}
+    <div className="animals">{images}</div>
+    <p id="fact"></p>
+  </div>
+);
+
+function displayFact(e) {
+  const animalName = e.target.alt;
+  const randomIndex = Math.floor(Math.random() * 3);
+  const funFact = animals[animalName].facts[randomIndex];
+  const factElement = document.getElementById("fact");
+  factElement.innerHTML = funFact;
+}
+
+ReactDOM.render(animalFacts, document.getElementById('root'));
+
+
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +58,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
